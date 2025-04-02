a97a4622afe1a4fd396646cd42dfdfba1b0a2701
Adivya Tripathi <adivyatripathi93@gmail.com>
2025-04-02 21:43:00 +0530

    Time: 32 ms (5.13%), Space: 16.4 MB (100%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        import math
+        if str1+str2 != str2+str1:
+            return ""
+        lent = str1[:math.gcd(len(str1),len(str2))]
+        return lent
+        
