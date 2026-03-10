17fdf2340b640d4e9bcf3661762d45fe9046e5df
Adivya Tripathi <adivyatripathi93@gmail.com>
2026-03-11 01:36:40 +0530

    Time: 8 ms (53.37%), Space: 19.3 MB (83.35%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        reverse = 0
+        first = x
+        while x > 0:
+            reverse = (reverse*10)+(x%10)
+            x//=10
+        print(reverse)
+
+        return reverse == first
